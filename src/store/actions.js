e9867b4c4905d75001fbf403f83b3265397a338a
arthurrubens <arth.rubens@gmail.com>
2019-05-02 00:34:22 +0200

    TopNews panel load is tmp. fixed.

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,8 +5,8 @@ Vue.use(VueResource);
 
 export default {
     loadTopHeadlines(context) {
-        var country = "",
-            topNewsApiUrl = `https://newsapi.org/v2/top-headlines?country=${context.state.country}&apiKey=${context.state.apiKey}`;
+        var country = "us",
+            topNewsApiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${context.state.apiKey}`;
         Vue.http.get(topNewsApiUrl).then(response => {
             context.commit('setTopHeadlines', response.body);
         });
@@ -15,7 +15,6 @@ export default {
     loadCategoryHeadlines(context) {
         var urlParamsString = context.getters.urlParamsString,
             topNewsApiUrl = `https://newsapi.org/v2/top-headlines?${urlParamsString}`;
-            console.log(urlParamsString);
         Vue.http.get(topNewsApiUrl).then(response => {
             context.commit('setCategoryHeadlines', response.body);
         });
